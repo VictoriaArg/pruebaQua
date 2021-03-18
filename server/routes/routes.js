9dad9e4e26f043aa9b309801307ca1fcfe898a73
VictoriaArg <argasvic@gmail.com>
2021-03-18 15:10:44 -0300

    entrega final

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,12 +1,12 @@
 const server = require('express').Router();
-const dataFilter = require('../helper/dataFilter');
+const { dataFilter } = require('../helper/dataFilter');
 
 server.get('/hello', (req, res) => res.send('Hello Uruguay!'));
 
 server.get('/', (req, res) => {
     (function() {
         try { 
-            const data = dataFilter.dataFilter()
+            const data = dataFilter()
             if (data) {
                 res.status(200).json(data);
             } else {
